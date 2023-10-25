d43bdf227d0dde5e040290131288e9a3e7e43bd8
Tomasz Niedzialkowski <tomasz.niedzialkowski@unitedrobots.co>
2023-10-25 17:08:15 +0200

    first_commit

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,40 @@
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+    public:
+
+    double x;
+    double y;
+
+    Wektor2D(){
+        x = 0;
+        y = 0;
+    };
+
+    Wektor2D(double xx, double yy) : x(xx), y(yy){};
+
+    void setX(double new_x){
+        x = new_x;
+    };
+
+    double getX(){
+        return x;
+    };
+
+    void setY(double new_y){
+        y = new_y;
+    };
+
+    double getY(){
+        return y;
+    };
+
+    Wektor2D operator+(Wektor2D& b){
+        double sum_x = x + b.x;
+        double sum_y = y + b.y;
+        return Wektor2D(sum_x, sum_y);
+    };
+
+    double operator*(Wektor2D& b){
+        return x * b.x + y * b.y;
+    };
 };
